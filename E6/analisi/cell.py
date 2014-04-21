59b765c33084346deec5741652d2a544c9e967ae
DejavuITA <Bazzanella.Davide@gmail.com>
2014-04-21 14:48:27 +0200

    quasi finita

--- E6/analisi/cell.py
+++ E6/analisi/cell.py
@@ -4,6 +4,9 @@ from matplotlib import *		# da matplotlib importo tutto # non devo utilizzare il
 import matplotlib.pyplot as plt
 import numpy as np			# importo numpy come np
 
+from mpl_toolkits.axes_grid1 import host_subplot
+import mpl_toolkits.axisartist as AA
+
 data1 = np.genfromtxt("../dati/night_cell_back.csv", delimiter=',', skip_header=1, names=True)
 data2 = np.genfromtxt("../dati/night_cell_fw.csv", delimiter=',', skip_header=1, names=True)
 nightV1 = data1['VDC']
@@ -26,12 +29,14 @@ f1.suptitle("Caratteristica volt-amperometrica di una cella fotovoltaica",
     y=0.97, fontsize=17)
 
 # GRAFICO 1
-ax1 = f1.add_subplot(1, 1, 1)
+ax1 = host_subplot(111, axes_class=AA.Axes)
 
 # crea plot con le barre d'errore (o anche senza)
 
 ax1.axhline(y=0, c='black', linewidth=1)
 ax1.axvline(x=0, c='black', linewidth=1)
+ax1.axhline(y=-25.5, xmin=0.865, xmax=1, c='black',linewidth=1,linestyle='dashed')
+ax1.axvline(x=5.75, ymin=0, ymax=0.4875, c='black',linewidth=1,linestyle='dashed')
 
 night = ax1.errorbar(x=nightV1, y=nightI1,
     fmt='o--', c='blue', linewidth=2)
@@ -46,21 +51,43 @@ ax1.errorbar(x=lightV2, y=lightI2,
     fmt='o--', c='red', linewidth=2)
 
 ax1.axhline(y=-20.6, xmin=0.865, xmax=1, c='black', linewidth=1)
-ax1.axvline(x=4.80, ymin=0, ymax=0.485, c='black', linewidth=1)
+ax1.axvline(x=4.80, ymin=0, ymax=0.4875, c='black', linewidth=1)
 
 ax1.set_xlabel(u'd.d.p. [V]',
     labelpad=8, fontsize=16)
 ax1.set_ylabel(u'Intensità di corrente [mA]',
     labelpad=-4, fontsize=16)
 
-ax1.text(-2.5, -19,r'$I_{@P_{max}}$', horizontalalignment='center', verticalalignment='center', fontsize=21)
-ax1.text(4.7, -90,r'$V_{@P_{max}}$', horizontalalignment='right', verticalalignment='center', fontsize=21)
+ax1.text(8.3, -16,r'$I_{@P_{max}}$', horizontalalignment='left', verticalalignment='center', fontsize=20)
+ax1.text(4.8, -108,r'$V_{@P_{max}}$', horizontalalignment='center', verticalalignment='top', fontsize=20)
+
+ax1.annotate(r'$I_{sc}$', xy=(5.75, 0),  xycoords='data',
+                xytext=(5.75-10, 50), textcoords='data',
+		va='center', fontsize=20,
+                arrowprops=dict(arrowstyle="->",
+                                )
+                )
+ax1.annotate(r'$V_{oc}$', xy=(0, -25.5),  xycoords='data',
+                xytext=(-10, -25.5+50), textcoords='data',
+		va='center', fontsize=20,
+                arrowprops=dict(arrowstyle="->",
+                                )
+                )
 
 ax1.set_xlim((-51,8))
 ax1.set_ylim((-65,105))
 
-ax1.set_xticks((-50,-40,-30,-20,-10,0,4.8))
-ax1.set_yticks((-100,-50,-20.6,0,50,100))
+ax1.set_xticks((-50,-40,-30,-20,-10,0,4.8,5.75))
+ax1.set_xticklabels(('-50','-40','-30','-20','-10','0','4.8',''))
+ax1.set_yticks((-100,-50,-25.5,-20.6,0,50,100))
+ax1.set_yticklabels(('-100','-50','','-20.6','0','50','100'))
+
+# ax2 is responsible for "top" axis and "right" axis
+ax2 = ax1.twin()
+ax2.set_yticks((-100,-50,-25.5,-20.6,0,50,100))
+ax2.set_yticklabels(("","","-25.5",))
+ax2.set_xticks((-50,-40,-30,-20,-10,0,4.8,5.75))
+ax2.set_xticklabels(('','','','','','','','5.75'))
 
 ax1.grid(True)
 
@@ -68,8 +95,8 @@ ax1.grid(True)
 ax1.legend((night, light), ("cella solare protetta da radiazioni", "cella solare esposta a luce"), 'upper left', prop={'size': 16})
 
 # questo imposta i bordi del grafico
-f1.subplots_adjust(left=0.09, right=0.97,
-    top=0.92, bottom=0.10, hspace=0.08, wspace=0)
+f1.subplots_adjust(left=0.09, right=0.93,
+    top=0.9, bottom=0.10, hspace=0.08, wspace=0)
 
 # mostra grafico
 plt.show()
--- E6/analisi/ripple.py
+++ E6/analisi/ripple.py
@@ -4,6 +4,17 @@ from matplotlib import *		# da matplotlib importo tutto # non devo utilizzare il
 import matplotlib.pyplot as plt
 import numpy as np			# importo numpy come np
 
+from mpl_toolkits.axes_grid1 import host_subplot
+import mpl_toolkits.axisartist as AA
+
+data1 = np.genfromtxt("../dati/n1.csv", delimiter=',', skip_header=1, names=True)
+t1 = data1['second']
+V1 = data1['Volt']
+
+data3 = np.genfromtxt("../dati/n3.csv", delimiter=',', skip_header=1, names=True)
+t3 = data3['second']
+V3 = data3['Volt']
+
 data4 = np.genfromtxt("../dati/n4.csv", delimiter=',', skip_header=1, names=True)
 t4 = data4['second']
 V4 = data4['Volt']
@@ -20,42 +31,79 @@ data7 = np.genfromtxt("../dati/n7.csv", delimiter=',', skip_header=1, names=True
 t7 = data7['second']
 V7 = data7['Volt']
 
+data11 = np.genfromtxt("../dati/n11.csv", delimiter=',', skip_header=1,names=True)
+t11 = data11['second']
+V11 = data11['Volt']
+
+data12 = np.genfromtxt("../dati/n12.csv", delimiter=',', skip_header=1,names=True)
+t12 = data12['second']
+V12 = data12['Volt']
+
+data13 = np.genfromtxt("../dati/n13.csv", delimiter=',', skip_header=1,names=True)
+t13 = data13['second']
+V13 = data13['Volt']
+
 rcParams['font.size'] = 15
 # Creo un grafico la dimensione è in pollici
 f1 = plt.figure(figsize=(12, 7), dpi=65)
 # Titolo del grafico
-f1.suptitle("Caratteristica volt-amperometrica di un diodo 1N4007",
+f1.suptitle("Ponte di Graetz: segnale stabilizzato",
     y=0.97, fontsize=17)
 
 # GRAFICO 1
-ax1 = f1.add_subplot(1, 1, 1)
+ax1 = host_subplot(111, axes_class=AA.Axes)
 
 # crea plot con le barre d'errore (o anche senza)
 
-dati4 = ax1.errorbar(x=t4, y=V4, fmt='-', c='blue', linewidth=2)
-dati5 = ax1.errorbar(x=t5, y=V5, fmt='x-', c='blue', linewidth=2)
+ax1.axhline(y=8.06, c='black', linewidth=1)
+ax1.axhline(y=9.72424631, c='black', linewidth=1, linestyle='dashed')
+ax1.axhline(y=5.3, c='black', linewidth=1, linestyle='dashed')
 
-dati6 = ax1.errorbar(x=t6, y=V6,
-    fmt='-', c='blue', linewidth=2)
-#    markersize=7, markeredgewidth=1)
-dati7 = ax1.errorbar(x=t7, y=V7,
-    fmt='-', c='#000000', linewidth=2)
+ax1.annotate('', xy=(-5, 9.7),  xycoords='data',
+                xytext=(-5, 5.3), textcoords='data', va='center',
+                arrowprops=dict(arrowstyle="<->",
+                                )
+                )
+ax1.text(-5+0.3,7.5, r'$V_{AC}$', fontsize=16,va='center', ha='left')
+ax1.text(18.3,8.1, r'$V_{DC}$', fontsize=16,va='bottom', ha='left')
 
-ax1.set_xlabel(u'Tempo [s]',
+#dati1 = ax1.errorbar(x=t1, y=V1, fmt='-', c='#00FF00', linewidth=2)
+
+	##dati3 = ax1.errorbar(x=t3, y=V3, fmt='-', c='#00AA00', linewidth=2)
+dati4 = ax1.errorbar(x=t4*1000, y=V4, fmt='-', c='#777777', linewidth=2)
+#dati5 = ax1.errorbar(x=t5, y=V5, fmt='-', c='#8888FF', linewidth=2)
+#dati6 = ax1.errorbar(x=t6, y=V6, fmt='-', c='#FF00BB', linewidth=2)
+dati7 = ax1.errorbar(x=t7*1000, y=9.72424631+0.95*(V7-9.72424631), fmt='-', c='#FF0000', linewidth=2)
+
+	##dati11 = ax1.errorbar(x=t11, y=V11, fmt='-', c='#00FFFF', linewidth=2)
+	##dati12 = ax1.errorbar(x=t12, y=V12, fmt='-', c='#0000AA', linewidth=2)
+	##dati13 = ax1.errorbar(x=t13, y=V13, fmt='-', c='#00AAAA', linewidth=2)
+
+ax1.set_xlabel(u'Tempo [ms]',
     labelpad=8, fontsize=16)
 ax1.set_ylabel(u'd.d.p. [V]',
     labelpad=0, fontsize=16)
 
-ax1.set_xlim((-0.01,0.01))
-ax1.set_ylim((-0.1,10.5))
+ax1.set_xlim((-20,20))
+ax1.set_ylim((-0.7,10.5))
+
+#ax1.set_xticks((-50,-40,-30,-20,-10,0,4.8))
+#ax1.set_yticks((0,2,4,6,8,8.06,10))
+
+# ax2 is responsible for "top" axis and "right" axis
+ax2 = ax1.twin()
+ax2.set_yticks((8.06,))
+#ax2.set_xticklabels(("5.7",))
+#ax2.set_xticklabels(["$0$", r"$\frac{1}{2}\pi$",r"$\pi$", r"$\frac{3}{2}\pi$", r"$2\pi$"])
+ax2.axis["top"].major_ticklabels.set_visible(False)
 
 ax1.grid(True)
 
 # questo produce una legenda
-ax1.legend((dati6, dati7), ("Ia serie di dati", "IIa serie di dati"), 'upper left', prop={'size': 16})
+ax1.legend((dati4, dati7), ("Segnale in input", "Segnale in output"), 'lower right', prop={'size': 16})
 
 # questo imposta i bordi del grafico
-f1.subplots_adjust(left=0.08, right=0.97,
+f1.subplots_adjust(left=0.07, right=0.94,
     top=0.92, bottom=0.10, hspace=0.08, wspace=0)
 
 # mostra grafico
