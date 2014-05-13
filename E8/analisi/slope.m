dcfca684a45b1617299ace8f2e93f6db9d7f490a
alessandromarty <ale.50special@hotmail.it>
2014-05-13 17:19:20 +0200

    asdfwe

--- E8/analisi/slope.m
+++ E8/analisi/slope.m
@@ -4,13 +4,13 @@ source("../../algoritmi/utils.m");
 
 Ib = csvread("../dati/circ1.csv")(3:15, 1);
 Ic = csvread("../dati/circ1.csv")(3:15, 2);
-Ib = Ic .- Ib;
-dIb = Ib./1000;
-dIc = Ic./1000;
+Ic = Ic .- Ib;
+dIb = Ib./50;
+dIc = Ic./50;
 
 %w = ones(length(Ic)-10,1).*0.01^(-2);
 w = dIb.^(-2);
 
-[A, B, sA, sB] = fit(Ib, Ic, w) % w = dy^(-2)
+[A, B, sA, sB] = fit(Ic, Ib, w) % w = dy^(-2)
 
 display("");
